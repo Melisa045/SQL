a126eef983b555951543d7ac83552abb4ac7d2fa
Melisa045 <131567991+Melisa045@users.noreply.github.com>
2024-02-01 15:26:46 -0500

    Add files via upload

---
+++ 06_usuarios_permisos.sql
@@ -0,0 +1,17 @@
+-- Usuario solo_lectura con permisos de solo lectura
+CREATE USER solo_lectura@floreria  IDENTIFIED BY '9273@]=maow';
+
+-- Conceder permisos de solo lectura en todas las tablas del marketplace_db. No se permite eliminar registros
+GRANT SELECT ON marketplace_db.* TO solo_lectura@floreria;
+SHOW GRANTS FOR solo_lectura@floreria ;
+
+-- Usuario lectura_insercion_modificacion con permisos de lectura, inserción y modificación
+CREATE USER 'lectura_insercion_modificacion'@floreria IDENTIFIED BY 'j28jw157h#';
+
+-- Conceder permisos de lectura, inserción y modificación en todas las tablas del marketplace_db. No se permite eliminar registros
+GRANT SELECT, INSERT, UPDATE ON marketplace_db.* TO 'lectura_insercion_modificacion'@floreria;
+SHOW GRANTS FOR lectura_insercion_modificacion@floreria;
+
+-- Eliminar la capacidad de eliminar registros para ambos usuarios y que se aplica a nivel global
+REVOKE DELETE ON *.* FROM solo_lectura@floreria ;
+REVOKE DELETE ON *.* FROM lectura_insercion_modificacion@floreria;
